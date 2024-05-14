919e7966a3a38f3c5d79f1145d25d5d88884f34c
francis-lp-win <franzdiaz460@gmail.com>
2024-05-14 09:56:56 +0400

    ref: fixed welcome box alignment and contained functions for input handlers

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import {
   remove_field_handler,
   toggle_multipage_input,
   update_config_ui,
-} from "./form_input_handlers.js";
+  set_task_active,
+} from "./input_handlers.js";
 import { create_task_component } from "./ui.js";
 import { init_session } from "./services/chrome_storage_api.js";
 const sidebar = document.getElementById("sidebar");
@@ -30,17 +31,6 @@ const task_schema_container = document.getElementById("task-schema-container");
 window.addEventListener("load", () => {
   init_session();
 });
-function set_task_active(data: {
-  target: HTMLElement;
-  task_list: Array<HTMLElement>;
-}) {
-  const current_active_task = data.task_list.find((i) =>
-    i.classList.contains("active"),
-  );
-  current_active_task?.classList.remove("active");
-  data.target.classList.add("active");
-  console.log(data.target);
-}
 
 Event_Signal.subscribe("update_task_ui", set_task_active);
 Event_Signal.subscribe("new_current_task", update_config_ui);
--- src/input_handlers.ts
+++ src/input_handlers.ts
@@ -2,21 +2,33 @@ import { create_input_field, multipage_inputs } from "./ui.js";
 import { find_top_parent } from "./utils/find_top_parent.js";
 import { t_task } from "./utils/types/project_types";
 
+export function set_task_active(data: {
+  target: HTMLElement;
+  task_list: Array<HTMLElement>;
+}) {
+  const current_active_task = data.task_list.find((i) =>
+    i.classList.contains("active"),
+  );
+  current_active_task?.classList.remove("active");
+  data.target.classList.add("active");
+  console.log(data.target);
+}
+
 // triggered when a new task is clicked
 export function update_config_ui(data: t_task) {
   const form = document.querySelector("form");
   const websiteURL_input = form?.querySelector("#websiteURL");
   const multipage_toggle = form?.querySelector(
-    `input#multipageToggle`
+    `input#multipageToggle`,
   ) as HTMLInputElement;
   const task_schema_inputs = Array.from(
-    form?.querySelectorAll(".task-schema-input") as NodeListOf<HTMLElement>
+    form?.querySelectorAll(".task-schema-input") as NodeListOf<HTMLElement>,
   );
   console.log({
     webURL: websiteURL_input,
     multipage_toggle: multipage_toggle.checked,
     task_schema_inputs: task_schema_inputs.map(
-      (inputs_container) => inputs_container.children
+      (inputs_container) => inputs_container.children,
     ),
   });
 
@@ -40,15 +52,15 @@ export function toggle_multipage_input(e: Event) {
 
 export function add_field_handler(e: Event) {
   const task_schema_container = document.getElementById(
-    "task-schema-container"
+    "task-schema-container",
   );
   const new_input_field = create_input_field().querySelector(
-    ".task-schema-input-container"
+    ".task-schema-input-container",
   );
   console.log(new_input_field);
   task_schema_container?.insertBefore(
     new_input_field as Element,
-    task_schema_container.children[1]
+    task_schema_container.children[1],
   );
 }
 
@@ -57,7 +69,7 @@ export function remove_field_handler(e: Event) {
   if (target.classList.contains("delete-field")) {
     const target_parent = find_top_parent(
       target,
-      "task-schema-input-container"
+      "task-schema-input-container",
     );
     target_parent?.remove();
   }
